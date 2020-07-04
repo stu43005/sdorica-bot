7cb5e22061e8e5fd9e95e5f3dada1904cf645928
Shiaupiau <stu43005@gmail.com>
2020-07-05 00:33:03 +0800

    feat(check-emoji): add reactions count

--- commands/owner/check-emoji.ts
+++ commands/owner/check-emoji.ts
@@ -11,7 +11,7 @@ export default class CheckEmojiCommand extends Command2 {
 			name: 'check-emoji',
 			group: 'owner',
 			memberName: 'check-emoji',
-			description: 'check-emoji',
+			description: '顯示最不常使用的 30 個表符',
 			ownerOnly: true,
 
 			args: [
@@ -19,12 +19,18 @@ export default class CheckEmojiCommand extends Command2 {
 					type: 'string',
 					key: 'guildId',
 					prompt: 'guild-id',
+				},
+				{
+					type: 'integer',
+					key: 'count',
+					prompt: 'count',
+					default: 30,
 				}
 			],
 		});
 	}
 
-	async run2(message: Discord.Message, { guildId }: { guildId: string }) {
+	async run2(message: Discord.Message, { guildId, count }: { guildId: string, count: number }) {
 
 		const guild = message.client.guilds.resolve(guildId);
 		if (!guild) {
@@ -44,30 +50,30 @@ export default class CheckEmojiCommand extends Command2 {
 			const weeklyData = weeklySnapshot.data() as any;
 			if (weeklyData) {
 				mergeRecordData(data, weeklyData, 'emojis');
+				if (weeklyData['reactions']) {
+					if (!data['emojis']) data['emojis'] = {};
+					Object.keys(weeklyData['reactions']).forEach(key => {
+						data['emojis'][key] = (data['emojis'][key] || 0) + weeklyData['reactions'][key];
+					});
+				}
 			}
 		}
 
 		let data2: {
-			id: string,
-			name: string,
 			count: number,
 			emoji: Discord.GuildEmoji,
 		}[] = [];
 		guild.emojis.cache.forEach((emoji) => {
 			data2.push({
-				id: emoji.id,
-				name: emoji.name,
 				count: data.emojis[emoji.id] || 0,
 				emoji
 			});
 		});
-		data2 = data2.sort((a, b) => a.count - b.count);
+		data2 = data2.sort((a, b) => b.count - a.count).slice(data2.length - count);
 
-		const data3: string[] = data2.filter(e => e.count > 0).map(e => {
-			return `${e.emoji}: ${e.count}`;
-		}).concat(['', 'No use:']).concat(data2.filter(e => e.count == 0).map(e => {
-			return `${e.emoji}`;
-		}).join(' '));
+		const data3: string[] = data2.map(e => {
+			return `${e.emoji.animated ? ':a' : ''}:${e.emoji.name}: ${e.count}`;
+		});
 
 		return message.say(data3.join('\n'), {
 			split: true,

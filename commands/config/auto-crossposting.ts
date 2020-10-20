a9b2921a8a251f82720f09b7944a74b35cc37067
Shiaupiau <stu43005@gmail.com>
2020-10-20 21:19:23 +0800

    feat(auto-crossposting): new command!

---
+++ commands/config/auto-crossposting.ts
@@ -0,0 +1,63 @@
+import * as Discord from "discord.js";
+import { CommandoClient } from "discord.js-commando";
+import { Command2 } from "../../typings/discord.js-commando/command";
+import { arrayConcat } from "../../utils";
+
+const key = 'autoCrossposting';
+
+export default class AutoCrosspostingCommand extends Command2 {
+	constructor(client: CommandoClient) {
+		super(client, {
+			name: 'auto-crossposting',
+			group: 'config',
+			memberName: 'auto-crossposting',
+			description: '設定自動發佈貼文(必須是公告頻道)',
+			guildOnly: true,
+			userPermissions: ['MANAGE_GUILD'],
+
+			args: [
+				{
+					key: 'channel',
+					type: 'news-channel',
+					prompt: '請輸入一個公告頻道',
+				}
+			],
+		});
+
+		client.on('message', async (message: Discord.Message) => {
+			const guild = message.guild;
+			if (!guild || !this.isEnabledIn(guild)) return;
+
+			if (message.channel.type !== 'news') return;
+
+			const channelId = message.channel.id;
+			const list: string[] = guild.settings.get(key);
+			if (list.indexOf(channelId) != -1) {
+				await message.crosspost();
+			}
+		});
+	}
+
+	async run2(message: Discord.Message, { channel }: { channel: Discord.NewsChannel }) {
+		if (!message.guild) return null;
+		let result: Discord.Message[] = [];
+
+		const list: string[] = message.guild.settings.get(key);
+		const channelId = channel.id;
+
+		const index = list.indexOf(channelId);
+		if (index != -1) {
+			// found, remove it
+			list.splice(index, 1);
+			result = arrayConcat(result, await message.say(`<#${channel.id}> 將不會自動發佈貼文`));
+		} else {
+			// add it
+			list.push(channelId);
+			result = arrayConcat(result, await message.say(`<#${channel.id}> 將會自動發佈貼文`));
+		}
+
+		await message.guild.settings.set(key, list);
+		return result;
+	}
+
+}
---
+++ types/news-channel.ts
@@ -0,0 +1,62 @@
+import { GuildChannel, Util } from "discord.js";
+import { Argument, ArgumentType, CommandoClient, CommandoMessage, util } from "discord.js-commando";
+
+export default class NewsChannelArgumentType extends ArgumentType {
+	constructor(client: CommandoClient) {
+		super(client, 'news-channel');
+	}
+
+	validate(val: string, msg: CommandoMessage, arg: Argument) {
+		const matches = val.match(/^(?:<#)?([0-9]+)>?$/);
+		if (matches) {
+			try {
+				const channel = msg.client.channels.resolve(matches[1]);
+				if (!channel || channel.type !== 'news') return false;
+				if (arg.oneOf && !arg.oneOf.includes(channel.id)) return false;
+				return true;
+			} catch (err) {
+				return false;
+			}
+		}
+		if (!msg.guild) return false;
+		const search = val.toLowerCase();
+		let channels = msg.guild.channels.cache.filter(channelFilterInexact(search));
+		if (channels.size === 0) return false;
+		if (channels.size === 1) {
+			if (arg.oneOf && !arg.oneOf.includes(channels.first()?.id)) return false;
+			return true;
+		}
+		const exactChannels = channels.filter(channelFilterExact(search));
+		if (exactChannels.size === 1) {
+			if (arg.oneOf && !arg.oneOf.includes(exactChannels.first()?.id)) return false;
+			return true;
+		}
+		if (exactChannels.size > 0) channels = exactChannels;
+		return channels.size <= 15 ?
+			`${util.disambiguation(
+				channels.map(chan => Util.escapeMarkdown(chan.name)), 'news channels'
+			)}\n` :
+			'Multiple news channels found. Please be more specific.';
+	}
+
+	parse(val: string, msg: CommandoMessage) {
+		const matches = val.match(/^(?:<#)?([0-9]+)>?$/);
+		if (matches) return msg.client.channels.resolve(matches[1]) || null;
+		if (!msg.guild) return null;
+		const search = val.toLowerCase();
+		const channels = msg.guild.channels.cache.filter(channelFilterInexact(search));
+		if (channels.size === 0) return null;
+		if (channels.size === 1) return channels.first();
+		const exactChannels = channels.filter(channelFilterExact(search));
+		if (exactChannels.size === 1) return exactChannels.first();
+		return null;
+	}
+}
+
+function channelFilterExact(search: string) {
+	return (chan: GuildChannel) => chan.type === 'news' && chan.name.toLowerCase() === search;
+}
+
+function channelFilterInexact(search: string) {
+	return (chan: GuildChannel) => chan.type === 'news' && chan.name.toLowerCase().includes(search);
+}
